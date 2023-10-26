a7d7b68a52698b07366636d63b214a241d989df6
Ahsanul Karim Tanim <tanim494@gmail.com>
2023-10-27 01:17:05 +0600

    Update working

--- app/src/main/java/com/tanim/uniguide/MainActivity.java
+++ app/src/main/java/com/tanim/uniguide/MainActivity.java
@@ -1,26 +1,43 @@
 package com.tanim.uniguide;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigation;
     TextView toTxt;
+    private DrawerLayout drawerLayout;
+    private ImageView menuIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menuIcon = findViewById(R.id.menuIcon);
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Open the left side drawer
+                drawerLayout.openDrawer(findViewById(R.id.navigationView));
+            }
+        });
+
 
         bottomNavigation  = findViewById(R.id.bottomNavigation);
         toTxt = findViewById(R.id.toolText);
@@ -37,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 if (item.getItemId() == R.id.nv_home) {
-                    Toast.makeText(MainActivity.this, "Introduction Section", Toast.LENGTH_SHORT).show();
 
                     FragmentManager fgMan = getSupportFragmentManager();
                     FragmentTransaction tran = fgMan.beginTransaction();
